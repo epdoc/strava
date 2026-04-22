ea2e3017c83b713c4d5f5dd0f1cfdbd8bb0904dd
Jim Pravetz <jpravetz@gmail.com>
2026-04-21 21:19:49 -0600

    feat: Update dependencies and improve activity retrieval
    - Update @epdoc/daterange to ^2.1.14
    - Update @epdoc/datetime to ^3.2.10
    - Refresh token before fetching activities
    - Improve logging during activity retrieval

--- packages/app/src/consts.ts
+++ packages/app/src/consts.ts
@@ -6,6 +6,7 @@ export default {
     'clientCreds': FS.File.config('epdoc', 'strava', 'client.creds.json'),
     'userSegments': FS.File.config('epdoc', 'strava', 'user.segments.json'),
     'userSettings': FS.File.config('epdoc', 'strava', 'user.settings.json'),
+    'userRegions': FS.File.config('epdoc', 'strava', 'user.regions.json'),
     'userCreds': FS.File.config('epdoc', 'strava', 'user.creds.json'),
     'userState': FS.File.config('epdoc', 'strava', 'user.state.json'),
   },

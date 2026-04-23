20bd709b268afb207ff3c9b112cf653fd82ea514
Jim Pravetz <jpravetz@gmail.com>
2026-04-22 18:49:13 -0600

    Minor

--- packages/api/src/types.ts
+++ packages/api/src/types.ts
@@ -94,22 +94,6 @@ export type StravaCredsData = {
   };
 };
 
-/**
- * A filter for activities.
- *
- * This can be used to filter a list of activities based on various criteria.
- */
-export type ActivityFilter = {
-  /** Whether to include only commute activities. */
-  commuteOnly?: boolean;
-  /** Whether to include only non-commute activities. */
-  nonCommuteOnly?: boolean;
-  /** An array of activity types to include. */
-  include?: string[];
-  /** An array of activity types to exclude. */
-  exclude?: string[];
-};
-
 /** Data for a segment effort, as returned by the Strava API. */
 export type SegmentData = Schema.Segment.DetailedEffort; // Changed to DetailedSegmentEffort
 /** A segment effort. */

c30667c9bab7633abcf40986f69820750bd76149
James Pravetz <jpravetz@epdoc.com>
2026-07-29 17:45:44 -0600

    feat: add build info to strava context initialization

--- packages/strava/main.ts
+++ packages/strava/main.ts
@@ -1,10 +1,12 @@
 import * as CliApp from '@epdoc/cliapp';
 import { Ctx } from '@epdoc/strava-core';
+import build from './build/build-info.json' with { type: 'json' };
 import pkg from './deno.json' with { type: 'json' };
 import { RootCommand } from './src/mod.ts';
+const buildInfo = build as CliApp.BuildInfoFile;
 
 if (import.meta.main) {
-  const ctx = new Ctx.Context(pkg);
+  const ctx = new Ctx.Context({ ...pkg, ...buildInfo });
   await ctx.setupLogging({ pkg: 'strava' });
 
   const cmd = new RootCommand(ctx, { root: { dryRun: true, trace: false } });
